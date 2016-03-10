e5add715fa30749b8f0d2c690850734bced61bc6
delspoune <spoune@hotmail.fr>
2016-03-10 10:29:55 +0100

    Ajout du main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
-/*
- * Permet la récupération du nom du fichier selectionné
- * Et l'affiche dans le input
- */
-$(document).on('change', '.btn-file :file', function() {
-  var input = $(this),
-      numFiles = input.get(0).files ? input.get(0).files.length : 1,
-      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-  input.trigger('fileselect', [numFiles, label]);
-});
-
-$(document).ready( function() {
-    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-        
-        var input = $(this).parents('.input-group').find(':text'),
-            log = numFiles > 1 ? numFiles + ' files selected' : label;
-        
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) console.log(log);
-        }
-    });
+$(document).ready(function(){
+	$("#addEnfant").click(function(){
+		$("#ajoutEnfant").show();
+		$('#ajoutAdherent').hide();
+      });
+  		
 });
